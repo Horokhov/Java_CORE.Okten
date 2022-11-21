32d4888c8723b6dea3f240bf03af73d8007ee3b6
MaxHorokhov <maxhorokhov@gmail.com>
2022-11-21 15:56:19 +0200

    Lesson_2.1

--- src/third/Main.java
+++ src/third/Main.java
@@ -1,7 +1,17 @@
 package third;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        new User(1,
+                "vasya",
+                "pupkin",
+                "asd@asd.com",
+                31,
+                Gender.MALE,
+                Arrays.asList(new Skills("title",1),new Skills("title",1)),
+                new Car("toyota",2001,250));
     }
 }
--- src/third/User.java
+++ src/third/User.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,5 +17,25 @@ public class User {
     private String email;
     private int age;
     private Gender gender;
+    private List<Skills> skills=new ArrayList<>();
     private Car car;
+
+    public User(int id, String name, String surname, String email, int age, Gender gender, List<Skills> skills, Car car) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.skills = skills;
+        this.car = car;
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Skills {
+    private String title;
+    private int exp;
 }
