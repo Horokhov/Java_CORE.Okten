2e225288093455920c230278094ce0aa1d0bed58
MaxHorokhov <maxhorokhov@gmail.com>
2022-11-23 01:05:47 +0200

    Lesson_2.1

--- src/additional/Comments.java
+++ src/additional/Comments.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class Comments {
     private int id;
     private String title;
     private String body;
+
+    private String comment;
 
     public Comments(int userId, int id, String title, String body) {
         this.userId = userId;
--- src/additional/Main.java
+++ src/additional/Main.java
@@ -2,17 +2,18 @@ package additional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Comments> comment =new ArrayList<Comments>();
+        ArrayList<Comments> comment =new ArrayList<>();
         comment.add(new Comments(1, 1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit","quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"));
         comment.add(new Comments(1, 2, "qui est esse", "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"));
         comment.add(new Comments(1, 3, "ea molestias quasi exercitationem repellat qui ipsa sit aut", "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"));
 
 
-        ArrayList<Posts> post=new ArrayList<Posts>();
+        ArrayList<Posts> post=new ArrayList<>();
    post.add(new Posts(1,1,"id labore ex et quam laborum","Eliseo@gardner.biz","laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"));
            post.add(new Posts(1,2,"quo vero reiciendis velit similique earum","Jayne_Kuhic@sydney.com","est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"));
                 post.add(new Posts(1,3,"odio adipisci rerum aut animi","Nikita@garfield.biz","quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"));
@@ -21,10 +22,17 @@ public class Main {
                 post.add(new Posts(3,13,"aut inventore non pariatur sit vitae voluptatem sapiente","Kariane@jadyn.tv","fuga eos qui dolor rerum\ninventore corporis exercitationem\ncorporis cupiditate et deserunt recusandae est sed quis culpa\neum maiores corporis et"));
                 post.add(new Posts(3,14,"et officiis id praesentium hic aut ipsa dolorem repudiandae","Nathan@solon.io","vel quae voluptas qui exercitationem\nvoluptatibus unde sed\nminima et qui ipsam aspernatur\nexpedita magnam laudantium et et quaerat ut qui dolorum"));
                 post.add(new Posts(3,15,"debitis magnam hic odit aut ullam nostrum tenetur","Maynard.Hodkiewicz@roberta.com","nihil ut voluptates blanditiis autem odio dicta rerum\nquisquam saepe et est\nsunt quasi nemo laudantium deserunt\nmolestias tempora quo quia"));
-        for (Posts posts : Com) {
 
+        for (Posts posts : post) {
+            posts.getId();
+            return;
         }
 
+        for (Comments comments : comment){
+            if(comments.getId())
         }
+
     }
+
+    };
 
