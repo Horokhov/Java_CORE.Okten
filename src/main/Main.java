0ab21cfc47478014a7bdfb6e3d169da9dfc1ed93
MaxHorokhov <maxhorokhov@gmail.com>
2022-11-24 00:43:40 +0200

    Lesson_3

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Printable book1=new Book("Edgar","Poem");
-        Printable book2=new Book("Ethan","Ludwig");
-        Printable magazine1=new Magazine("Lethal","Tempo");
-        Printable magazine2=new Magazine("Guide","Anxious");
-
+        ArrayList<Printable> collection=new ArrayList<>();
+        collection.add(new Book("Edgar","Poem"));
+        collection.add(new Book("Ethan","Ludwig"));
+        collection.add(new Magazine("Lethal","Tempo"));
+        collection.add(new Magazine("Guide","Anxious"));
+        System.out.println(collection);
 
 
     }
