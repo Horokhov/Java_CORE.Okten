0c7907c9722afe979188da24d73681ea44793a99
MaxHorokhov <maxhorokhov@gmail.com>
2022-12-02 06:52:51 +0200

    Lesson_4

---
+++ src/fourth_zoooo/Main.java
@@ -0,0 +1,43 @@
+package fourth_zoooo;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        class Club {
+            private Map<Person, List<Pet>> map = new LinkedHashMap<>();
+            //1) додати учасника в клуб;
+            public void newPerson(Person person) {
+                map.put(new Person(1,"Max","Dr"), null);
+            }
+            //2) додати тваринку до учасника клубу.
+            public void newPet(Person personId,Pet pet){
+                Set<Map.Entry<Person, List<Pet>>> entrySet = map.entrySet();
+                Iterator<Map.Entry<Person, List<Pet>>> iterator= entrySet.iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry<Person, List<Pet>> next =  iterator.next();
+                    if (next.getKey().getId() == personId.getId()) {
+                        next.setValue((List<Pet>) new Pet(1,"Bob",3));
+
+                    }
+                }
+            }
+//3) видалити тваринку з власника.
+        public void removePet(int personId, Pet pet ) {
+                Set<Person> people=map.keySet();
+            for (Person person : people) {
+                if(person.getId()==personId) {
+                    List<Pet> pets=map.get(person);
+                    pets.remove(pet);
+                }
+            }
+        }
+//4) видалити учасника клубу.
+            public void removePerson(Person person) {
+                map.get(person).removeIf(p->p.equals(person));
+            }
+//5) видалити конкретну тваринку з усіх власників.
+// 6) вивести на екран зооклуб.
+        }
+    }
+}
